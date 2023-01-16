2416cdf00802fc24cd7fc5566141d32c18cad33d
humayonreza <smhumayonreza@gmaikl.com>
2023-01-16 22:01:41 +1100

    add backend service to home.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private backendService: BackendService) {}
 
   ngOnInit(): void {}
 }
